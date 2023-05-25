a583b05538002ca3c0c138a8be0767a0fd100271
Arnaud Lachaume <arnaud.lachaume@keypup.io>
2023-05-25 17:19:28 +0200

    add logging metric alert naming definition

--- ScoutSuite/providers/gcp/resources/stackdrivermonitoring/monitoring_alert_policies.py
+++ ScoutSuite/providers/gcp/resources/stackdrivermonitoring/monitoring_alert_policies.py
@@ -14,22 +14,19 @@ class MonitoringAlertPolicies(Resources):
 
     def _parse_alert_policy(self, raw_alert_policies):
         alert_policy_dict = {}
-        alert_policy_dict['project_ownership_assignments'] = \
-            self._specific_alert_policy_present(raw_alert_policies)
-        alert_policy_dict['audit_config_change'] = self._specific_alert_policy_present(raw_alert_policies)
-        alert_policy_dict['custom_role_change'] = self._specific_alert_policy_present(raw_alert_policies)
-        alert_policy_dict['vpc_network_firewall_rule_change'] = self._specific_alert_policy_present(raw_alert_policies)
-        alert_policy_dict['vpc_network_route_change'] = self._specific_alert_policy_present(raw_alert_policies)
-        alert_policy_dict['vpc_network_change'] = self._specific_alert_policy_present(raw_alert_policies)
-        alert_policy_dict['cloud_storage_iam_permission_change'] = \
-            self._specific_alert_policy_present(raw_alert_policies)
-        alert_policy_dict['sql_instance_conf_change'] = self._specific_alert_policy_present(raw_alert_policies)
+        alert_policy_dict['cloud_storage_iam_permission_change'] = self._specific_alert_policy_present(raw_alert_policies, 'audit-cloud-storage-iam-permission-changes')
+        alert_policy_dict['audit_config_change'] = self._specific_alert_policy_present(raw_alert_policies, 'audit-configuration-changes')
+        alert_policy_dict['custom_role_change'] = self._specific_alert_policy_present(raw_alert_policies, 'audit-custom-role-changes')
+        alert_policy_dict['project_ownership_assignments'] = self._specific_alert_policy_present(raw_alert_policies, 'audit-project-ownership-assignments-changes')
+        alert_policy_dict['sql_instance_conf_change'] = self._specific_alert_policy_present(raw_alert_policies, 'audit-sql-instance-configuration-changes')
+        alert_policy_dict['vpc_network_change'] = self._specific_alert_policy_present(raw_alert_policies, 'audit-vpc-network-changes')
+        alert_policy_dict['vpc_network_firewall_rule_change'] = self._specific_alert_policy_present(raw_alert_policies, 'audit-vpc-network-firewall-rule-changes')
+        alert_policy_dict['vpc_network_route_change'] = self._specific_alert_policy_present(raw_alert_policies, 'audit-vpc-network-route-changes')
         return alert_policy_dict
 
-    def _specific_alert_policy_present(self, alert_policies):
+    def _specific_alert_policy_present(self, alert_policies, logging_metric_name):
         for alert_policy in alert_policies:
             for condition in alert_policy.conditions:
-                if condition.condition_threshold.filter == 'metric.type=\"logging.googleapis.com/user/<Log Metric ' \
-                                                           'Name>\"' and alert_policy.enabled.value:
+                if (f'metric.type = "logging.googleapis.com/user/{logging_metric_name}"' in condition.condition_threshold.filter) and alert_policy.enabled.value:
                     return True
         return False
